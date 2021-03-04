c6354bbe71f9bfa04d690c08b780a7e637a5b1b7
rohith0696 <60017598+rohith0696@users.noreply.github.com>
2021-03-03 19:07:31 -0600

    code for color 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,30 @@
+import locationsArray from '../locations.js';
+
+let locationElement = document.getElementById("bgrtwo");
+let locationElement2 = document.getElementById("bgrone");
+
+window.addEventListener('load', main);
+
+locationElement.addEventListener('click', locationHandler);
+locationElement.addEventListener('touch', locationHandler);
+locationElement2.addEventListener('click', colorFunction1);
+locationElement2.addEventListener('touch', colorFunction1);
+
+function main() {
+    console.log('Page is fully loaded');
+}
+
+
+
 function colorFunction1() {
-    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", ];
-    const random = Math.floor(Math.random() * values.length);
-    let value = values[random];
-    document.getElementById("bgrone").style.backgroundColor = "#7aeb7a";
-    document.getElementById("lbl").innerHTML = value;
-    let utterance = new SpeechSynthesisUtterance(`     You have picked the card of color Light Green of thevalue     ${value}`);
-    speechSynthesis.speak(utterance);
+    document.getElementById("targetloc").innerHTML = "The Treasure is in the location ";
+    document.getElementById("lbl").innerHTML = targetLoc.Name;
+    document.getElementById("device-lat1").innerHTML = targetLoc.coordinates[0].latitude;
+    document.getElementById("device-long1").innerHTML = targetLoc.coordinates[0].longitude;
+    // let utterance = new SpeechSynthesisUtterance(`The location where the treasure is ${targetLoc.Name}`);
+    // speechSynthesis.speak(utterance);
+
+
 }
 
 function colorFunction2() {
@@ -16,4 +35,7 @@ function colorFunction2() {
     document.getElementById("lbl2").innerHTML = value;
     let utterance = new SpeechSynthesisUtterance(`     You have picked the card of color Light Blue of the value     ${value}`);
     speechSynthesis.speak(utterance);
+}
+
+async function locationHandler() {
 }
