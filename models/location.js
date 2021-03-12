2aae5743ca72cfe2997f9ed69edd9f1c692c4539
Vishal Reddy <59984658+Vishalreddy114@users.noreply.github.com>
2021-03-12 09:09:34 -0600

    updated location.js model

---
+++ models/location.js
@@ -0,0 +1,37 @@
+/**
+ *  Location model
+ *  It describes each attribute.
+ 
+ * 
+ */
+module.exports = (db, DataTypes) => {
+  db.define('Location', {
+    locationId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+      
+    },
+    locationName:{
+      type: DataTypes.STRING(30),
+      unique: true,
+      allowNull: false,
+      required: true
+      
+    },
+    locationLatitude: {
+      type: DataTypes.DECIMAL(10, 8),
+      allowNull: false
+    },
+    locationLongitude: {
+      type: DataTypes.DECIMAL(11, 8),
+      allowNull: false
+    },
+
+    locationValue: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
+
+};
