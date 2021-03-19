ad01fc20ef2a2a1594216c414d4f4ec038d946b7
vishal Reddy <vishalreddy0507@gmail.com>
2021-03-18 23:32:20 -0500

    updated locationController.js

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -1,163 +1,171 @@
-// const db = require('../models/index')();
+/**
+ * Location controller
+ *  Handles requests related to location (see routes)
+ *
+ *
+ */
 
-// const tabTitle = 'Locations';
+const LOG = require('../util/logger');
+const db = require('../models/index')();
 
-// // FUNCTIONS TO RESPOND WITH JSON DATA  ----------------------------------------
+const tabTitle = 'Locations';
 
-// // GET all JSON
-// module.exports.findAll = async (req, res) => {
-//   (await db).models.Location.findAll({
-//     attributes: {
-//       exclude: ['createdAt', 'updatedAt'],
-//     },
-//     include: [
-//       {
-//         model: (await db).models.Location,
-//         attributes: ['id', 'name'],
-//       },
-//     ],
-//   })
-//     .then((data) => {
-//       res.send(data);
-//     })
-//     .catch((err) => {
-//       res.status(500).send({
-//         message: err.message || 'Error retrieving all.',
-//       });
-//     });
-// };
+// FUNCTIONS TO RESPOND WITH JSON DATA  ----------------------------------------
 
-// // GET one JSON by ID
-// module.exports.findOne = async (req, res) => {
-//   const { id } = req.params;
-//   (await db).models.Location.findByPk(id)
-//     .then((data) => {
-//       res.send(data);
-//     })
-//     .catch((err) => {
-//       res.status(500).send({
-//         message: `Error retrieving item with id=${id}: ${err.message}`,
-//       });
-//     });
-// };
+// GET all JSON
+module.exports.findAll = async (req, res) => {
+  (await db).models.Location.findAll({
+    attributes: {
+      exclude: ['createdAt', 'updatedAt'],
+    },
+    include: [
+      {
+        model: (await db).models.Location,
+        attributes: ['id', 'name'],
+      },
+    ],
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Error retrieving all.',
+      });
+    });
+};
 
-// // HANDLE EXECUTE DATA MODIFICATION REQUESTS -----------------------------------
+// GET one JSON by ID
+module.exports.findOne = async (req, res) => {
+  const { id } = req.params;
+  (await db).models.Location.findByPk(id)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: `Error retrieving item with id=${id}: ${err.message}`,
+      });
+    });
+};
 
-// // POST /save
-// module.exports.saveNew = async (req, res) => {
-//   try {
-//     const context = await db;
-//     await context.models.Location.create(req.body);
-//     return res.redirect('/location');
-//   } catch (err) {
-//     return res.redirect('/location');
-//   }
-// };
+// HANDLE EXECUTE DATA MODIFICATION REQUESTS -----------------------------------
 
-// // POST /save/:id
-// module.exports.saveEdit = async (req, res) => {
-//   try {
-//     const reqId = parseInt(req.params.id, 10);
-//     const context = await db;
-//     const updated = await context.models.Location.update(req.body, {
-//       where: { id: reqId },
-//     });
-//     console.log(`Updated: ${JSON.stringify(updated)}`);
-//     return res.redirect('/location');
-//   } catch (err) {
-//     return res.redirect('/location');
-//   }
-// };
+// POST /save
+module.exports.saveNew = async (req, res) => {
+  try {
+    const context = await db;
+    await context.models.Location.create(req.body);
+    return res.redirect('/location');
+  } catch (err) {
+    return res.redirect('/location');
+  }
+};
 
-// // POST /delete/:id
-// module.exports.deleteItem = async (req, res) => {
-//   try {
-//     const reqId = parseInt(req.params.id, 10);
-//     const deleted = (await db).models.Location.destroy({
-//       where: { id: reqId },
-//     });
-//     if (deleted) {
-//       return res.redirect('/location');
-//     }
-//     throw new Error(`${reqId} not found`);
-//   } catch (err) {
-//     return res.status(500).send(err.message);
-//   }
-// };
+// POST /save/:id
+module.exports.saveEdit = async (req, res) => {
+  try {
+    const reqId = parseInt(req.params.id, 10);
+    const context = await db;
+    const updated = await context.models.Location.update(req.body, {
+      where: { id: reqId },
+    });
+    LOG.info(`Updated: ${JSON.stringify(updated)}`);
+    return res.redirect('/location');
+  } catch (err) {
+    return res.redirect('/location');
+  }
+};
 
-// // RESPOND WITH VIEWS  --------------------------------------------
+// POST /delete/:id
+module.exports.deleteItem = async (req, res) => {
+  try {
+    const reqId = parseInt(req.params.id, 10);
+    const deleted = (await db).models.Location.destroy({
+      where: { id: reqId },
+    });
+    if (deleted) {
+      return res.redirect('/location');
+    }
+    throw new Error(`${reqId} not found`);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+};
 
-// // GET to this controller base URI (the default)
-// module.exports.showIndex = async (req, res) => {
-//   (await db).models.Location.findAll()
-//     .then((data) => {
-//       res.locals.locations = data;
-//       res.render('location/index.ejs', { title: tabTitle, res });
-//     })
-//     .catch((err) => {
-//       res.status(500).send({
-//         message: err.message || 'Error retrieving all.',
-//       });
-//     });
-// };
+// RESPOND WITH VIEWS  --------------------------------------------
 
-// // GET /create
-// module.exports.showCreate = async (req, res) => {
-//   // create a temporary item and add it to the response.locals object
-//   // this also provides a place to pass any validation errors to the view
-//   // Important! attributes must match those defined in the model
-//   const tempItem = {
-//     name: 'LocationName',
-//   };
-//   res.locals.location = tempItem;
-//   res.render('location/create.ejs', { title: tabTitle, res });
-// };
+// GET to this controller base URI (the default)
+module.exports.showIndex = async (req, res) => {
+  (await db).models.Location.findAll()
+    .then((data) => {
+      res.locals.locations = data;
+      res.render('location/index.ejs', { title: tabTitle, res });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Error retrieving all.',
+      });
+    });
+};
 
-// // GET /delete/:id
-// module.exports.showDelete = async (req, res) => {
-//   const { id } = req.params;
-//   (await db).models.Location.findByPk(id)
-//     .then((data) => {
-//       res.locals.location = data;
-//       if (data) {
-//         res.render('location/delete.ejs', { title: tabTitle, res });
-//       } else {
-//         res.redirect('location/');
-//       }
-//     })
-//     .catch((err) => {
-//       res.status(500).send({
-//         message: `Error retrieving item with id=${id}: ${err.message}`,
-//       });
-//     });
-// };
+// GET /create
+module.exports.showCreate = async (req, res) => {
+  // create a temporary item and add it to the response.locals object
+  // this also provides a place to pass any validation errors to the view
+  // Important! attributes must match those defined in the model
+  const tempItem = {
+    name: 'LocationName',
+  };
+  res.locals.location = tempItem;
+  res.render('location/create.ejs', { title: tabTitle, res });
+};
 
-// // GET /details/:id
-// module.exports.showDetails = async (req, res) => {
-//   const { id } = req.params;
-//   (await db).models.Location.findByPk(id)
-//     .then((data) => {
-//       res.locals.location = data;
-//       res.render('location/details.ejs', { title: tabTitle, res });
-//     })
-//     .catch((err) => {
-//       res.status(500).send({
-//         message: `Error retrieving item with id=${id}: ${err.message}`,
-//       });
-//     });
-// };
+// GET /delete/:id
+module.exports.showDelete = async (req, res) => {
+  const { id } = req.params;
+  (await db).models.Location.findByPk(id)
+    .then((data) => {
+      res.locals.location = data;
+      if (data) {
+        res.render('location/delete.ejs', { title: tabTitle, res });
+      } else {
+        res.redirect('location/');
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: `Error retrieving item with id=${id}: ${err.message}`,
+      });
+    });
+};
 
-// // GET /edit/:id
-// module.exports.showEdit = async (req, res) => {
-//   const { id } = req.params;
-//   (await db).models.Location.findByPk(id)
-//     .then((data) => {
-//       res.locals.location = data;
-//       res.render('location/edit.ejs', { title: tabTitle, res });
-//     })
-//     .catch((err) => {
-//       res.status(500).send({
-//         message: `Error retrieving item with id=${id}: ${err.message}`,
-//       });
-//     });
-// };
+// GET /details/:id
+module.exports.showDetails = async (req, res) => {
+  const { id } = req.params;
+  (await db).models.Location.findByPk(id)
+    .then((data) => {
+      res.locals.location = data;
+      res.render('location/details.ejs', { title: tabTitle, res });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: `Error retrieving item with id=${id}: ${err.message}`,
+      });
+    });
+};
+
+// GET /edit/:id
+module.exports.showEdit = async (req, res) => {
+  const { id } = req.params;
+  (await db).models.Location.findByPk(id)
+    .then((data) => {
+      res.locals.location = data;
+      res.render('location/edit.ejs', { title: tabTitle, res });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: `Error retrieving item with id=${id}: ${err.message}`,
+      });
+    });
+};
