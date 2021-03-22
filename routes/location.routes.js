e3b408e11c5760757bcc9759e831fce1f5ab7840
Vishal Reddy <59984658+Vishalreddy114@users.noreply.github.com>
2021-03-22 04:26:09 -0500

    updated location.routes.js

--- routes/location.routes.js
+++ routes/location.routes.js
@@ -1,33 +1,35 @@
-// const router = require('express').Router();
-// const controller = require('../controllers/locationController.js');
-// // const LOG = require('../util/logger');
+const router = require('express').Router();
+const controller = require('../controllers/locationController.js');
+const LOG = require('../util/logger');
 
-// console.log('Starting location routing.');
+LOG.info('Starting location routing.');
 
-// // -----------------------------------------------------------------------------
-// // match each expeced HTTP verb + URL endpoint request
-// // with a custom function to handle it
-// // -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// match each expeced HTTP verb + URL endpoint request
+// with a custom function to handle it
+// -----------------------------------------------------------------------------
 
-// // handle two requests for JSON (HTTP GET)
+// handle two requests for JSON (HTTP GET)
 
-// router.get('/findall', controller.findAll);
-// router.get('/findone/:id', controller.findOne);
+router.get('/findall', controller.findAll);
+router.get('/findone/:locationId', controller.findOne);
 
-// // handle three requests to perform database actions (HTTP POST)
+// handle three requests to perform database actions (HTTP POST)
 
-// router.post('/save', controller.saveNew);
-// router.post('/save/:id', controller.saveEdit);
-// router.post('/delete/:id', controller.deleteItem);
+router.post('/save', controller.saveNew);
+router.post('/save/:locationId', controller.saveEdit);
+router.post('/delete/:locationId', controller.deleteItem);
 
-// // handle five requests for webpages (HTTP GET)
+// handle five requests for webpages (HTTP GET)
 
-// router.get('/', controller.showIndex);
-// router.get('/create', controller.showCreate);
-// router.get('/details/:id', controller.showDetails);
-// router.get('/edit/:id', controller.showEdit);
-// router.get('/delete/:id', controller.showDelete);
+router.get('/', controller.showIndex);
+router.get('/create', controller.showCreate);
+router.get('/details/:locationId', controller.showDetails);
+router.get('/edit/:locationId', controller.showEdit);
+router.get('/delete/:locationId', controller.showDelete);
 
-// console.log('Loaded locations routes.');
+router.get('/location',controller.showIndex);
 
-// module.exports = router;
+LOG.info('Loaded locations routes.');
+
+module.exports = router;
