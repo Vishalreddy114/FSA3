76d5d2747cf0e9850623b86184e15a97e9ff3913
rohith0696 <60017598+rohith0696@users.noreply.github.com>
2021-03-24 03:00:18 -0500

    added various changes on public folder and views

---
+++ public/public/scripts/init-locations.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        Name: 'Horizons West Apartments',
+        type: 'circle',
+        latitude: 41.314477,
+        longitude: -95.056609
+    },
+    {
+        Name: 'Colden Pond',
+        type: 'circle',
+        latutide: 40.351126,
+        longitude: -94.882202
+    },
+    {
+        Name: 'B.D Owens Library',
+        type: 'circle',
+        Latitude: 40.353802,
+        Longitude: -94.885984
+
+    }
+
+];
---
+++ public/public/scripts/speech.js
@@ -0,0 +1,4 @@
+export default [
+    {status_message : "Select the location"},
+    {status1_message : "You are outside the target location."}
+]
